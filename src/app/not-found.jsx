cf2d1eb438bbbc255f038e909fa89f0c822d0cf1
zakaryaahmad1 <133588377+zakaryaahmad1@users.noreply.github.com>
2024-09-17 20:33:12 +0300

    fixed 404 page not found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,9 +1,11 @@
+import Link from 'next/link';
 import React from 'react';
 
 const NotFound = () => {
     return (
-        <div className='mt-40 text-black text-center m-auto w-full text-7xl'>
-           404 Page not found 
+        <div className='mt-40 font-bold flex flex-col italic text-[#AD343E] text-center m-auto w-full text-7xl'>
+           <p>404 Page not found </p>
+           <Link className='underline mt-10 text-4xl' href={"/"}>Back Home</Link>
         </div>
     );
 }
