022fb3fa15f443d70eba017f8646c3d11d633347
zakaryaahmad1 <133588377+zakaryaahmad1@users.noreply.github.com>
2024-09-17 20:27:36 +0300

    fixed 404 page not found

---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='mt-40 text-black text-center m-auto w-full text-7xl'>
+           404 Page not found 
+        </div>
+    );
+}
+
+export default NotFound;
